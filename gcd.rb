e3b55cb5fb3c9c7a7244a6fa569a9c5f9fb471f7
BALLESTER HERNANDEZ <alu4371@cc175.(none)>
2013-10-18 14:57:58 +0100

    Maximo Comun Divisor (mcd) de gcd.rb corregido

--- gcd.rb
+++ gcd.rb
@@ -1,9 +1,10 @@
-def gcd(u, v)
+def gcd(u, v) #V SIEMPRE DEBE DE SER MENOR
   u, v = u.abs, v.abs
-  while v == 0
-    u, v = v, u % v
-  end
-  u
-end
 
-puts gcd(6,3)
+  while v != 0 
+    aux = u
+    u = v 
+    v = aux % v 
+  end
+  u   
+end
