773dc2776d318db69500cb76ce4986451e74f283
BALLESTER HERNANDEZ <alu4371@cc175.(none)>
2013-10-18 15:10:45 +0100

    Clase Racional: constructor

--- racional.rb
+++ racional.rb
@@ -1,6 +1,23 @@
+#!/usr/bin/ruby
+
 # Implementar en este fichero la clase para crear objetos racionales
 
 require "./gcd.rb"
 
 class Fraccion
+  #ACCESO ATRIBUTOS
+  attr_reader :n, :d 	  
+
+  #CONSTRUCTOR			
+  def initialize(n, d)
+    if d == 0
+	@n = n
+	puts "INTRODUZCA UN DENOMINADOR DISTINTO DE 0"
+	@d = gets.chomp	 #suprime /n
+    else
+      @n, @d = n, d 
+    end	
+  end
+
 end
+
