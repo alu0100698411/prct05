e203efd62822766750889d7df3e48375a79028d7
BALLESTER HERNANDEZ <alu4371@cc175.(none)>
2013-10-18 15:45:45 +0100

    Pruebas unitarias: suma

--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,8 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
-
+  def test_suma
+    assert_equal("60/14", Racional.new(8,2).sum(Racional.new(2,7)).to_s)
+  end
+  
 end
